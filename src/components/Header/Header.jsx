0975b59e616658a42ecf7fc12ceab63b6f5d6137
Mehedi Ali <mehedi1234@hotmail.co.uk>
2023-01-10 17:23:07 +0000

    Add inital files and folders

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <h1>
+      Anime<strong>Toshokan</strong>
+    </h1>
+  );
+};
+
+export default Header;
